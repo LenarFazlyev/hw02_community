5cbc4cab6b4a9ded1dc86a524749797a59802760
Lenar <lr.fazliev@gmail.com>
2023-02-02 16:44:29 +0300

    after second review

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Group, Post
 
 
@@ -25,7 +26,6 @@ class GroupAdmin(admin.ModelAdmin):
     )
     list_filter = ('title', 'slug',)
     search_fields = ('title',)
-    empty_value_display = '-пусто-'
 
 
 admin.site.register(Post, PostAdmin)
