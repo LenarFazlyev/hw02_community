6c93feff476de3bb84c814ab6cc3e0668fe9b836
Lenar <lr.fazliev@gmail.com>
2023-02-07 09:39:58 +0300

    before filters

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +78,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'Europe/Moscow'
 
