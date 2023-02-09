caf78dfbeb28937ea9caa5d5d75410adb0991de0
Lenar <lr.fazliev@gmail.com>
2023-02-09 09:06:01 +0300

    before Paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
 from .models import Group, Post
 
@@ -11,7 +12,7 @@ def index(request):
     }
     return render(request, 'posts/index.html', context)
 
-
+@login_required
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:settings.LIMITS_IN_PAGE]
